5fd9bf8c6493d8537f39a2352d27308add88e465
addygeek <ui22cs03@iiitsurat.ac.in>
2025-01-27 23:43:42 +0530

    twitter scapper

---
+++ Assignment 3 - Scapers/Twitter Scapper/main.py
@@ -0,0 +1,6 @@
+#lets build a twitter scapper
+# there is issues with scapting these sites which reqired login
+# twtitter is not open sources so it will require to login to get access to more data witch makes the data scaping hard
+
+print('Twitter Scapper')
+#for it there are many library 
